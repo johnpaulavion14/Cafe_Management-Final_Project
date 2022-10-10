fed4e6a2dd5e3474ebff7f90063bb8e31f2fce12
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-10 12:48:00 +0800

    Done with Order Transaction model test

--- app/models/order_transaction.rb
+++ app/models/order_transaction.rb
@@ -2,4 +2,7 @@ class OrderTransaction < ApplicationRecord
     serialize :orders, Array
     belongs_to :user
     has_many :product_sales
+
+    validates :orders, presence: true
+    # validates :orders[:product_name], presence: true
 end
--- spec/factories/order_transactions.rb
+++ spec/factories/order_transactions.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :order_transaction do
     user
-    orders { ['asdff']}
-    # orders { [{:product_name=>"donut", :quantity=>2, :price=>15.0},{:total_price=>33.6, :tax=>3.59}] }
+    orders { [{:product_name=>"donut", :quantity=>2, :price=>15.0},{:total_price=>33.6, :tax=>3.59}] }
   end
 end
