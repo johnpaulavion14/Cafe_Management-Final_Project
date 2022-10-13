0fae18560e1dbcae55341bf5836eba676467e928
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-13 10:52:53 +0800

    Change Product request test

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     sequence(:product_name) {|n| "string#{n}"}
     price {100.00}
     image {"image"}
+    id {1}
   end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,24 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   describe "#Product Request" do
+    before do
+      sign_in create(:user)
+    end
     it "product index page" do
       get products_path
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(200)
     end
 
     it "create new product page " do
       get new_product_path
-      expect(response).to have_http_status(302)
-    end
-
-    it "edit product page" do
-      get edit_product_path(1)
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(200)
     end
 
     it "see all product page" do
       get edit_all_products_path
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(200)
     end
 
   end
