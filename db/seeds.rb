2b9330c78292f85245e2dded4755a2962359235b
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-11-02 12:58:11 +0800

    Add  sample seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.last.orders.create(orders: [{:product_name=>"Pizza", :quantity=>1, :price=>125.0}, {:total_price=>140.00, :tax=>15.00}], created_at:Date.today-2, updated_at:Date.today-2)
+
+User.last.products.find_by(product_name:"Pizza").product_solds.create(
+product_name: "Pizza",
+price: 125.0,
+quantity: 1,
+tax: 15.0,
+total_price: 140.0,
+created_at:Date.today-2,
+updated_at:Date.today-2
+
+)
